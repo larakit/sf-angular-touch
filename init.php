e6d318e08250371ec3b30e964532c7e47b99f7a2
Berdnikov Alexey <aberdnikov@gmail.com>
2017-04-20 09:00:39 +0500

    Update init.php

--- init.php
+++ init.php
@@ -2,5 +2,4 @@
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-touch')
     ->setSourceDir('public')
     ->usePackage('larakit/sf-angular')
-    ->ngModule('ngTouch')
     ->jsPackage('angular-touch.min.js');
